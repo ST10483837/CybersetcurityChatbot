6cc45e3ac33f748de99116000d7d74eb75a7f85f
ST10483837 <apfenengome@gmail.com>
2026-04-10 10:59:05 +0200

    Set up project structure and main files

---
+++ CybersetcurityChatbot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CybersecurityChatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cybersecurity Chatbot - Initial Setup");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
